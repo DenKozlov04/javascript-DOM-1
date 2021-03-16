7b7736b467513bcfdf4df8ce77b56331da4ae6d5
DenKozlov04 <datlovdenis363@gmail.com>
2021-03-16 15:56:20 +0200

    5 uzd

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,4 +1,4 @@
-document.addEventListener('click', function(e){
+document.addEventListener('click', function(){
   document.getElementById('more_text_content').style.display = 'block';
   document.getElementById('more_text_link').style.visibility = 'hidden';
 
